e13ffbee64463a4ed3af85558859f25ee4f959a9
Taejin Jung <taejin5314@gmail.com>
2021-08-10 21:06:51 -0400

    refactoring

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -29,17 +29,28 @@ const data = [
 
 class Dashboard extends Component {
   state = {
-    loading: false
+    loading: false,
+    focused: null
   };
 
+  selectPanel(id) {
+    this.setState((previousState) => ({
+      focused: previousState.focused !== null ? null : id
+    }));
+  }
+
   render() {
-    const dashboardClasses = classnames('dashboard');
+    const dashboardClasses = classnames('dashboard', {
+      'dashboard--focused': this.state.focused
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    const panels = data.map((panel) => <Panel key={panel.id} id={panel.id} label={panel.label} value={panel.value} />);
+    const panels = data
+      .filter((panel) => this.state.focused === null || this.state.focused === panel.id)
+      .map((panel) => <Panel key={panel.id} label={panel.label} value={panel.value} onSelect={(event) => this.selectPanel(panel.id)} />);
 
     return <main className={dashboardClasses}>{panels}</main>;
   }
